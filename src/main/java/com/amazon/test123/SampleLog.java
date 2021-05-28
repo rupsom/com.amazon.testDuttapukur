b657c2b6b3385cee0f30461db434724876aaef1c
AmazonRR <rupamrouth.it@gmail.com>
2021-05-28 18:08:41 +0530

    First commit o

---
+++ src/main/java/com/amazon/test123/SampleLog.java
@@ -0,0 +1,16 @@
+package com.amazon.test123;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class SampleLog {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Logger logger = LogManager.getLogger(SampleLog.class);
+		
+		logger.info("Hi the weather is too good");
+	}
+
+}
